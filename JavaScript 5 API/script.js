76be92139e5489710696283ed599f566b329de09
Kamalesh Karthick <76035160+kamaleshkarthick@users.noreply.github.com>
2024-03-05 16:40:15 +0530

    js

--- JavaScript 5 API/script.js
+++ JavaScript 5 API/script.js
@@ -1,14 +1,39 @@
-let url=" http://3.7.50.225/api/admin/get-all-sessions"
-let xhr = new XMLHttpRequest();
+let url = " http://3.7.50.225/api/admin/get-all-sessions";
+// let xhr = new XMLHttpRequest();
 
-xhr.open("GET", url);
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(JSON.parse(this.response))
-    }
-    else {
-        console.log("Error")
-    }
-}
-xhr.send();
+// xhr.open("GET", url);
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//         console.log(JSON.parse(this.response))
+//     }
+//     else {
+//         console.log("Error")
+//     }
+// }
+// xhr.send();
 
+fetch(url)
+  .then((res) => {
+    // console.log(res);
+    // console.log(res.json());
+    return res.json();
+  })
+  .then((data) => {
+      console.log(data);
+    let tabvalue;
+    data.forEach(values => {
+     // console.log(values);
+      tabvalue+= `<tr>
+      <th scope="row">${values.session_id}</th>
+      <td>${values.user_id}</td>
+      <td>${values.name}</td>
+      <td>${values.phone}</td>
+      <td>${values.email_id}</td>
+      <td>${values.created_at}</td>
+      <td>${values.approved}</td>
+      <td>${values.ipaddress}</td>
+  </tr> `;
+    });
+    //   console.log(tabvalue)
+    document.getElementById("tbody").innerHTML = tabvalue;
+  });
