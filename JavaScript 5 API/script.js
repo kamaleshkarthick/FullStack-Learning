0f8bf048e909ae061340549f7a8508a4d552b3db
Kamalesh Karthick <76035160+kamaleshkarthick@users.noreply.github.com>
2024-03-05 17:40:59 +0530

    api

--- JavaScript 5 API/script.js
+++ JavaScript 5 API/script.js
@@ -12,28 +12,82 @@ let url = " http://3.7.50.225/api/admin/get-all-sessions";
 // }
 // xhr.send();
 
+// fetch(url)
+//   .then((res) => {
+//     // console.log(res);
+//     // console.log(res.json());
+//     return res.json();
+//   })
+//   .then((data) => {
+//       console.log(data);
+//     let tabvalue;
+//     data.forEach(values => {
+//       // console.log(values);
+
+//       tabvalue+= `<tr>
+//       <th scope="row">${values.session_id}</th>
+//       <td>${values.user_id}</td>
+//       <td>${values.name}</td>
+//       <td>${values.phone}</td>
+//       <td>${values.email_id}</td>
+//       <td>${values.created_at}</td>
+//       <td>${values.approved}</td>
+//       <td>${values.ipaddress}</td>
+//   </tr> `;
+//     });
+//     //   console.log(tabvalue)
+//     document.getElementById("tbody").innerHTML = tabvalue;
+//   });
+
 fetch(url)
   .then((res) => {
-    // console.log(res);
-    // console.log(res.json());
     return res.json();
   })
   .then((data) => {
-      console.log(data);
-    let tabvalue;
-    data.forEach(values => {
-     // console.log(values);
-      tabvalue+= `<tr>
-      <th scope="row">${values.session_id}</th>
-      <td>${values.user_id}</td>
-      <td>${values.name}</td>
-      <td>${values.phone}</td>
-      <td>${values.email_id}</td>
-      <td>${values.created_at}</td>
-      <td>${values.approved}</td>
-      <td>${values.ipaddress}</td>
-  </tr> `;
+    console.log(data);
+
+    data.forEach((values) => {
+      var tabvalue = document.createElement("tr");
+
+      tabvalue.innerHTML = `
+        <td scope="row">${values.session_id}</td>
+        <td>${values.user_id}</td>
+        <td>${values.name}</td>
+        <td>${values.phone}</td>
+        <td>${values.email_id}</td>
+        <td>${values.created_at}</td>
+        <td>${values.approved}</td>
+        <td>${values.ipaddress}</td>
+      `;
+
+      document.getElementById("tbody").appendChild(tabvalue);
     });
-    //   console.log(tabvalue)
-    document.getElementById("tbody").innerHTML = tabvalue;
   });
+
+// fetch(url)
+//   .then((res) => {
+//     // console.log(res);
+//     // console.log(res.json());
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+
+//     data.forEach((values) => {
+//       // console.log(values);
+//       let tabvalue;
+//       tabvalue = `<tr>
+//       <th >${values.session_id}</th>
+//       <td>${values.user_id}</td>
+//       <td>${values.name}</td>
+//       <td>${values.phone}</td>
+//       <td>${values.email_id}</td>
+//       <td>${values.created_at}</td>
+//       <td>${values.approved}</td>
+//       <td>${values.ipaddress}</td>
+//   </tr> `;
+//       console.log(tabvalue);
+//       document.getElementById("tbody").innerHTML=tabvalue;
+//     });
+//     //   console.log(tabvalue)
+//   });
